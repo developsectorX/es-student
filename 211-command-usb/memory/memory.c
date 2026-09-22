2a6293a49d1ba709af2a802de1649161f8f996a5
Yuri Steinberg <developsector@gmail.com>
2026-09-22 21:11:02 +0200

    Сделана п2.1.3

--- 211-command-usb/memory/memory.c
+++ 211-command-usb/memory/memory.c
@@ -31,47 +31,53 @@ static void row(const char* name, uintptr_t start, uintptr_t end)
 
 void mem_info(void)
 {
-    // Шапка таблицы
-    printf("%-10s %-10s %-10s %8s\n", "region", "start", "end", "size");
+    // --- Расчет размеров из символов компоновщика ---
+    unsigned boot2_sz = (unsigned)((uintptr_t)&__boot2_end__ - (uintptr_t)&__boot2_start__);
+    unsigned text_sz = (unsigned)((uintptr_t)&__etext - (uintptr_t)&__boot2_end__);
+    unsigned data_sz = (unsigned)((uintptr_t)&__data_end__ - (uintptr_t)&__data_start__);
+    unsigned bss_sz = (unsigned)((uintptr_t)&__bss_end__ - (uintptr_t)&__bss_start__);
+    unsigned heap_sz = (unsigned)((uintptr_t)&__HeapLimit - (uintptr_t)&__bss_end__);
+    unsigned stack_sz = (unsigned)((uintptr_t)&__StackTop - (uintptr_t)&__StackBottom);
 
-    // --- Регионы, заданные чипом/платой ---
+    unsigned flash_img_sz = boot2_sz + text_sz + data_sz;
+    unsigned flash_total_sz = (unsigned)PICO_FLASH_SIZE_BYTES;
+    unsigned flash_free_sz = flash_total_sz - flash_img_sz;
+    unsigned ram_used = data_sz + bss_sz;
+
+    // --- Вывод таблицы ---
+    printf("%-10s %-10s %-10s %8s\n", "area", "start", "end", "size");
+
+    // Железо
     row("flash", XIP_BASE, XIP_BASE + PICO_FLASH_SIZE_BYTES);
     row("sram", SRAM_BASE, SRAM_BASE + SRAM_SIZE);
     row("rom", ROM_BASE, ROM_BASE + ROM_SIZE);
 
-    // --- Секции образа (символы компоновщика) ---
+    // Секции Flash
     row("image", (uintptr_t)&__flash_binary_start, (uintptr_t)&__flash_binary_end);
-    row("free", (uintptr_t)&__flash_binary_end,
-        XIP_BASE + PICO_FLASH_SIZE_BYTES);
+    row("free", (uintptr_t)&__flash_binary_end, XIP_BASE + PICO_FLASH_SIZE_BYTES);
 
     row("boot2", (uintptr_t)&__boot2_start__, (uintptr_t)&__boot2_end__);
     row("text", (uintptr_t)&__boot2_end__, (uintptr_t)&__etext);
+    row("data flash", (uintptr_t)&__etext, (uintptr_t)&__etext + data_sz);
 
-    // .data хранится во флеше (начальные значения) и копируется в ОЗУ
-    row("data flash", (uintptr_t)&__etext,
-        (uintptr_t)&__etext + ((uintptr_t)&__data_end__ - (uintptr_t)&__data_start__));
+    // Секции RAM
     row("data ram", (uintptr_t)&__data_start__, (uintptr_t)&__data_end__);
     row("bss", (uintptr_t)&__bss_start__, (uintptr_t)&__bss_end__);
     row("heap", (uintptr_t)&__bss_end__, (uintptr_t)&__HeapLimit);
     row("stack", (uintptr_t)&__StackBottom, (uintptr_t)&__StackTop);
 
-    // --- Итоги ---
-    printf("total\n");
+    // --- Вывод итогов  ---
+    printf("\ntotal\n");
+    printf("  flash image    %5u = boot2 %u + text %u + data %u\n",
+        flash_img_sz, boot2_sz, text_sz, data_sz);
 
-    uintptr_t img_start = (uintptr_t)&__flash_binary_start;
-    uintptr_t img_end = (uintptr_t)&__flash_binary_end;
-    uintptr_t flash_end = XIP_BASE + PICO_FLASH_SIZE_BYTES;
+    printf("  flash free   %7u of %u\n",
+        flash_free_sz, flash_total_sz);
 
-    printf("flash image: %u bytes\n", (unsigned)(img_end - img_start));
-    printf("flash free:  %u bytes of %u\n",
-        (unsigned)(flash_end - img_end),
-        (unsigned)PICO_FLASH_SIZE_BYTES);
+    printf("  ram used        %5u = data %u + bss %u\n",
+        ram_used, data_sz, bss_sz);
 
-    uintptr_t ram_used = ((uintptr_t)&__data_end__ - (uintptr_t)&__data_start__)
-        + ((uintptr_t)&__bss_end__ - (uintptr_t)&__bss_start__);
-    printf("ram used (.data + .bss): %u bytes\n", (unsigned)ram_used);
-
-    uintptr_t ram_free = ((uintptr_t)&__HeapLimit - (uintptr_t)&__bss_end__)
-        + ((uintptr_t)&__StackTop - (uintptr_t)&__StackBottom);
-    printf("ram free (heap + stack): %u bytes\n", (unsigned)ram_free);
+    printf("  ram free      %6u for heap and %u for stack\n",
+        heap_sz, stack_sz);
 }
+
