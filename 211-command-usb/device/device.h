2e24d0f872ae72d043f37ca616495119365db1af
Yuri Steinberg <developsector@gmail.com>
2026-09-22 19:33:40 +0200

    Сделана п2.1.1

---
+++ 211-command-usb/device/device.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define DEVICE_NAME "211_command_usb"
+#define FIRMWARE_VERSION "1.0.0"
+
+#define DEVICE_PROJECT "211-command-usb"
+#define DEVICE_REPO "https://github.com/developsectorX/es-student"
+
+#ifndef DEVICE_BOARD
+#define DEVICE_BOARD "unknown"
+#endif
+
+void device_info(void);
+
